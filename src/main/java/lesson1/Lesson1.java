0dde8f8c244e6fed1912540870f2a31565142ecd
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2019-04-16 22:10:18 +0300

    Первичная подготовка к работе

---
+++ src/main/java/lesson1/Lesson1.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+/**
+ * Java, algoritmus.
+ *
+ * @author Anatoly Lebedev
+ * @version 1.0.0 16.04.2019
+ * @link https://github.com/Centnerman
+ */
+
+public class Lesson1 {
+}
