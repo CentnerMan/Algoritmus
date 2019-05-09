968e6064e5fd0cc4b4c079a9ce358dafa320a4d6
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2019-05-09 19:00:12 +0300

    Пример дерева из методички

---
+++ src/main/java/lesson6/manual/Node.java
@@ -0,0 +1,12 @@
+package ru.lebedev.se.lesson6;
+
+public class Node {
+    public Person person;
+    public Node leftChild;
+    public Node rightChild;
+
+    public void display() {
+        System.out.println("Name: " + person.name + ", age: " + person.age);
+    }
+
+}
